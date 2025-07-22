f282974da9ceaf39b1ff480d52338ba110d8e292
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-21 21:42:49 -0400

    utilizing the user context to show the cart dialog

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,11 +3,13 @@ import {useContext} from "react";
 import CartContext from "../store/CartContext.jsx";
 import {currencyFormatter} from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext)
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice+ item.quantity * item.price, 0);
-    return <Modal className={"cart"}>
+    return <Modal className={"cart"} open={userProgressCtx.progress === 'cart'}>
         <h2>Your Cart</h2>
         <ul>
             {
