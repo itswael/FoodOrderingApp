f1172027e566487aaca18604346396ff0942d89d
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-21 21:56:31 -0400

    added a close dialog handler

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,6 +9,11 @@ export default function Cart() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext)
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice+ item.quantity * item.price, 0);
+
+    function handleCloseCart() {
+        userProgressCtx.hideCart();
+    }
+
     return <Modal className={"cart"} open={userProgressCtx.progress === 'cart'}>
         <h2>Your Cart</h2>
         <ul>
@@ -18,8 +23,8 @@ export default function Cart() {
         </ul>
         <p className={"cart-total"}>{currencyFormatter.format(cartTotal)}</p>
         <p className={"modal-actions"}>
-            <Button textOnly>Close</Button>
-            <Button >Go to checkout</Button>
+            <Button textOnly onClick={handleCloseCart}>Close</Button>
+            <Button onClick={handleCloseCart}>Go to checkout</Button>
         </p>
     </Modal>
 
