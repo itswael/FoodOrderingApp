babeefe86f5b7ca9e5c7dc071cde7a2e8cc80376
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-22 21:53:20 -0400

    added checkout component to display a form as a modal

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,34 @@
+import Modal from "./UI/Modal.jsx";
+import {useContext} from "react";
+import CartContext from "../store/CartContext.jsx";
+import {currencyFormatter} from "../util/formatting.js";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import userProgressContext from "../store/UserProgressContext.jsx";
+
+export default function Checkout() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(userProgressContext)
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice+ item.quantity * item.price, 0);
+    function handleClose(){
+        userProgressCtx.hideCheckout();
+    }
+    return <Modal open={userProgressCtx.progress === 'checkout'}>
+        <form>
+            <h2>Checkout</h2>
+            <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+            <Input label={"Full name"} type={"text"} id={"full-name"}/>
+            <Input label={"Email"} type={"email"} id={"email"}/>
+            <Input label={"Phone"} type={"text"} id={"phone"}/>
+            <Input label={"Street"} type={"text"} id={"street"}/>
+            <div className={"control-row"}>
+                <Input label={"Postal code"} type={"number"} id={"postal-code"}/>
+                <Input label={"City"} type={"text"} id={"city"}/>
+            </div>
+            <p className={"modal-actions"}>
+                <Button type={"button"} onClick={handleClose} textOnly>Close</Button>
+                <Button>Submit Order</Button>
+            </p>
+        </form>
+    </Modal>
+}
