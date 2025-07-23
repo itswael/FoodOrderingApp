682e1b1c2a3d42565f92b4f116581fef2c12d8ce
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-22 20:07:03 -0400

    integrated with cart item component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import {currencyFormatter} from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
@@ -18,7 +19,15 @@ export default function Cart() {
         <h2>Your Cart</h2>
         <ul>
             {
-                cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)
+                cartCtx.items.map((item) =>
+                    <CartItem key={item.id}
+                              name={item.name}
+                              price={item.price}
+                              quantity={item.quantity}
+                              onDecrease={() => cartCtx.removeItem(item.id)}
+                              onIncrease={() => cartCtx.addItem(item)}
+                    />
+                )
             }
         </ul>
         <p className={"cart-total"}>{currencyFormatter.format(cartTotal)}</p>
