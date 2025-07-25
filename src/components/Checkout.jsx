fb79e79197bd33887af548c6a512a2f0bf40f06c
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-24 23:32:04 -0400

    cleaned imports and fixed modal loading

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,35 +1,55 @@
-import Modal from "./UI/Modal.jsx";
-import {useContext} from "react";
-import CartContext from "../store/CartContext.jsx";
-import {currencyFormatter} from "../util/formatting.js";
-import Input from "./UI/Input.jsx";
-import Button from "./UI/Button.jsx";
-import UserProgressContext from "../store/UserProgressContext.jsx";
+import { useContext } from 'react';
+
+import Modal from './UI/Modal.jsx';
+import CartContext from '../store/CartContext.jsx';
+import { currencyFormatter } from '../util/formatting.js';
+import Input from './UI/Input.jsx';
+import Button from './UI/Button.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext);
-    const userProgressCtx = useContext(UserProgressContext)
-    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice+ item.quantity * item.price, 0);
-    
-    function handleClose(){
+    const userProgressCtx = useContext(UserProgressContext);
+
+    const cartTotal = cartCtx.items.reduce(
+        (totalPrice, item) => totalPrice + item.quantity * item.price,
+        0
+    );
+
+    function handleClose() {
         userProgressCtx.hideCheckout();
     }
-    return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-        <form>
-            <h2>Checkout</h2>
-            <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-            <Input label={"Full name"} type={"text"} id={"full-name"}/>
-            <Input label={"Email"} type={"email"} id={"email"}/>
-            <Input label={"Phone"} type={"text"} id={"phone"}/>
-            <Input label={"Street"} type={"text"} id={"street"}/>
-            <div className={"control-row"}>
-                <Input label={"Postal code"} type={"number"} id={"postal-code"}/>
-                <Input label={"City"} type={"text"} id={"city"}/>
-            </div>
-            <p className={"modal-actions"}>
-                <Button type={"button"} onClick={handleClose} textOnly>Close</Button>
-                <Button>Submit Order</Button>
-            </p>
-        </form>
-    </Modal>
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
+
+
+    }
+
+    return (
+        <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+            <form onSubmit={handleSubmit}>
+                <h2>Checkout</h2>
+                <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="E-Mail Address" type="email" id="email" />
+                <Input label="Street" type="text" id="street" />
+                <div className="control-row">
+                    <Input label="Postal Code" type="text" id="postal-code" />
+                    <Input label="City" type="text" id="city" />
+                </div>
+
+                <p className="modal-actions">
+                    <Button type="button" textOnly onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button>Submit Order</Button>
+                </p>
+            </form>
+        </Modal>
+    );
 }
