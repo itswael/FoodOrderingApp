fcc95a9e1f585a425590438566d9fec200af4cf3
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-07-27 10:51:41 -0400

    optimized content

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,14 +1,22 @@
-import {currencyFormatter} from "../util/formatting.js";
+import { currencyFormatter } from '../util/formatting.js';
 
-export default function CartItem({name, quantity, price, onIncrease, onDecrease}) {
-    return <li className={"cart-item"}>
-        <p>
-            {name} - {quantity} X {currencyFormatter.format(price)}
-        </p>
-        <p className={"cart-item-actions"}>
-            <button onClick={onDecrease}>-</button>
-            <span>QTY</span>
-            <button onClick={onIncrease}>+</button>
-        </p>
-    </li>
+export default function CartItem({
+                                     name,
+                                     quantity,
+                                     price,
+                                     onIncrease,
+                                     onDecrease,
+                                 }) {
+    return (
+        <li className="cart-item">
+            <p>
+                {name} - {quantity} x {currencyFormatter.format(price)}
+            </p>
+            <p className="cart-item-actions">
+                <button onClick={onDecrease}>-</button>
+                <span>{quantity}</span>
+                <button onClick={onIncrease}>+</button>
+            </p>
+        </li>
+    );
 }
